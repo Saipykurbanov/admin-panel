f80162b462400bb46ef2611c4c9804acf8388eaa
Saipykurbanov <kurbanoviliyas@mail.ru>
2024-11-28 22:39:28 +0300

    upd

---
+++ src/pages/sing_in/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/sing_in.css';
+
+
+const SignIn = () => {
+    return (
+        <div className='sing_in'>
+            
+        </div>
+    );
+};
+
+export default SignIn;
